4d543dd3a6b2082f1bf24ca3c81e9b390bcf75bc
tuzlu07x <fatihtuzlu07@gmail.com>
2022-01-11 00:49:33 +0300

    jetstrap fixed.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,39 +14,36 @@
                 @csrf
 
                 <!-- Name -->
-                <div class="form-group">
+                <div class="mb-3">
                     <x-label for="name" :value="__('Name')" />
 
                     <x-input id="name" type="text" name="name" :value="old('name')" required autofocus />
                 </div>
 
                 <!-- Email Address -->
-                <div class="form-group">
+                <div class="mb-3">
                     <x-label for="email" :value="__('Email')" />
 
                     <x-input id="email" type="email" name="email" :value="old('email')" required />
                 </div>
 
                 <!-- Password -->
-                <div class="form-group">
+                <div class="mb-3">
                     <x-label for="password" :value="__('Password')" />
 
-                    <x-input id="password" type="password"
-                                    name="password"
-                                    required autocomplete="new-password" />
+                    <x-input id="password" type="password" name="password" required autocomplete="new-password" />
                 </div>
 
                 <!-- Confirm Password -->
-                <div class="form-group">
+                <div class="mb-3">
                     <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                    <x-input id="password_confirmation" type="password"
-                                    name="password_confirmation" required />
+                    <x-input id="password_confirmation" type="password" name="password_confirmation" required />
                 </div>
 
                 <div class="mb-0">
                     <div class="d-flex justify-content-end align-items-baseline">
-                        <a class="text-muted me-3 text-decoration-none" href="{{ route('login') }}">
+                        <a class="text-muted mr-3 text-decoration-none" href="{{ route('login') }}">
                             {{ __('Already registered?') }}
                         </a>
 
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,11 +1,11 @@
 @props(['id' => 'navbarDropdown'])
 
-<li {!! $attributes->merge(['class' => 'nav-item dropdown']) !!}>
-    <a id="{{ $id }}" href="#" class="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+<li class="nav-item dropdown">
+    <a id="{{ $id }}" {!! $attributes->merge(['class' => 'nav-link dropdown-toggle']) !!} role="button" data-toggle="dropdown" aria-expanded="false">
         {{ $trigger }}
     </a>
 
-    <div class="dropdown-menu dropdown-menu-right animate slideIn" aria-labelledby="{{ $id }}">
+    <div class="dropdown-menu dropdown-menu-end animate slideIn" aria-labelledby="{{ $id }}">
         {{ $content }}
     </div>
 </li>
