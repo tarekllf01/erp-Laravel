70d077bb0cdca2adf7115e6e7b33da9fcd72c44a
tuzlu07x <fatihtuzlu07@gmail.com>
2022-01-14 23:52:08 +0300

    product index yapıldı

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable=[
+        'name',
+        'unit',
+        'unit_price',
+    ];
+
     use HasFactory;
 }
