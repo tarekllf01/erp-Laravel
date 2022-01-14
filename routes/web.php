91de68c9914287fb109d28fbed89bbc678a43a9e
tuzlu07x <fatihtuzlu07@gmail.com>
2022-01-14 23:51:45 +0300

    product rota verildi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,3 +28,4 @@ Route::group([
     'middleware' => ['auth','IsAdmin'],'prefix' => 'admin',],function(){
         Route::resource('user', UserController::class);
 });
+Route::resource('product', ProductController::class);
