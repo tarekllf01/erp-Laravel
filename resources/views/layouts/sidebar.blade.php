43aec54708016f33ad5d2de304d76904f10df917
tuzlu07x <fatihtuzlu07@gmail.com>
2022-01-14 22:25:13 +0300

    sidelink fixed.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,11 @@
 <x-side-link href="{{ route('dashboard') }}" icon="fas fa-tachometer-alt" :active="request()->routeIs('dashboard.*')">
     {{ __('Dashboard') }}
 </x-side-link>
-
+@if(auth()->user()->type !== 'employee')
 <x-side-link href="{{ route('user.index') }}" :active="request()->routeIs('user.*')" icon="fas fa-building">
     {{ __('User Rolleri') }}
+</x-side-link>
+@endif
+<x-side-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard.*')" icon="fas fa-building">
+    {{ __('Giren Mallar') }}
 </x-side-link>
